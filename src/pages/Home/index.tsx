122e20751892ed22d7fbc27ade09557e292bca85
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-12-13 17:15:24 -0300

    feat: create main to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,20 @@ const Home = () => {
     <div className='h-screen' >
         <div className='h-[100vh]'>
             <Header hgt='20%'/>
-            
+            <div style={{backgroundImage: `url(${ImageMain})`}} className='h-[86%] object-center'>
+                <div className='h-full bg-[rgba(0,0,0,0.5)]'>
+                    <div className='flex h-full justify-center items-center'>
+                        <div style={{backgroundImage:`url(${LogoMain})`}} className='h-[450px] w-[450px] bg-no-repeat flex items-end justify-center gap-10'>
+                        <ButtonMain color='#FB8951' border={0}>
+                            Order Now
+                        </ButtonMain>
+                        <ButtonMain color='transparent' border={2}>
+                            Download App
+                        </ButtonMain>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
