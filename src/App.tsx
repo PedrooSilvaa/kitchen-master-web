565c5ca0f2df1d3b25112fec83dc919e32371a11
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-12-13 16:03:12 -0300

    feat: configure axios and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/router'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
